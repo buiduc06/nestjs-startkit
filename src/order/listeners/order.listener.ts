9746b6515fc05ecde8d4ea026e6c3de75daf7ce2
Bùi Ngọc Đức <buiduc1998@gmail.com>
2022-07-17 04:57:20 +0700

    feat: :sparkles: send mail when order complete

--- src/order/listeners/order.listener.ts
+++ src/order/listeners/order.listener.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { RedisService } from 'src/shared/redis.service';
 import { Order } from '../order.model';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class OrderListener {
-  constructor(private redisService: RedisService) {}
+  constructor(
+    private redisService: RedisService,
+    private mailerService: MailerService,
+  ) {}
 
   @OnEvent('order.completed')
   /**
@@ -16,7 +20,21 @@ export class OrderListener {
   async handleOrderCompletedEvent(order: Order) {
     const client = this.redisService.getClient();
 
+    /* Adding the ambassador_revenue to the user's name in the rankings sorted set. */
     client.zincrby('rankings', order.ambassador_revenue, order.user.name);
-    console.log('dispatch order.completed', order);
+
+    /* Sending an email to the admin when an order is completed. */
+    this.mailerService.sendMail({
+      to: 'ducbui.dev@gmail.com',
+      subject: 'An order has been completed',
+      html: `Order #${order.ambassador_revenue} with a total of #${order.total} has been completed`,
+    });
+
+    /* Sending an email to the ambassador when an order is completed. */
+    this.mailerService.sendMail({
+      to: order.ambassador_email,
+      subject: 'An order has been completed',
+      html: `You earned #${order.ambassador_revenue} from the link#${order.code}`,
+    });
   }
 }
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -11,6 +11,7 @@ import { ProductModule } from '../product/product.module';
 import { StripeModule } from 'nestjs-stripe';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OrderListener } from './listeners/order.listener';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -24,6 +25,19 @@ import { OrderListener } from './listeners/order.listener';
       useFactory: (configService: ConfigService) => ({
         apiKey: configService.get('STRIPE_KEY'),
         apiVersion: configService.get('STRIPE_API_VERSION'),
+      }),
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAIL_HOST'),
+          port: configService.get('MAIL_PORT'),
+        },
+        defaults: {
+          from: configService.get('MAIL_FROM'),
+        },
       }),
     }),
   ],
