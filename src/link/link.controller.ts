96ea23b333e070c961813df5802d5c59610c74e1
Bùi Ngọc Đức <buiduc1998@gmail.com>
2022-07-16 22:54:43 +0700

    feat(ambassador): :sparkles: added stats

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -12,7 +12,9 @@ import { AdminGuard } from 'src/auth/admin.guard';
 import { AmbassadorGuard } from 'src/auth/ambassador.guard';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthService } from 'src/auth/auth.service';
+import { Order } from 'src/order/order.model';
 import { Product } from 'src/product/product';
+import { Link } from './link.model';
 import { LinkService } from './link.service';
 
 @Controller()
@@ -47,6 +49,36 @@ export class LinkController {
       code: Math.random().toString(36).substring(6),
       user,
       products: products.map((id) => ({ id })),
+    });
+  }
+
+  @UseGuards(AmbassadorGuard)
+  @Get('ambassador/stats')
+  /**
+   * It returns a list of links, each with a count of completed orders and the total revenue from those
+   * orders
+   * @param {Request} request - Request - This is the request object that is passed to the controller.
+   * @returns An array of objects with the following properties:
+   * - code: the code of the link
+   * - count: the number of completed orders
+   * - revenue: the total revenue of the completed orders
+   */
+  async stats(@Req() request: Request) {
+    const user = await this.authService.user(request);
+
+    const links: Link[] = await this.linkService.find({
+      user,
+      relations: ['orders'],
+    });
+
+    return links.map((link) => {
+      const completedOrders: Order[] = link.orders.filter((o) => o.complete);
+
+      return {
+        code: link.code,
+        count: completedOrders.length,
+        revenue: completedOrders.reduce((s, o) => s + o.ambassador_revenue, 0),
+      };
     });
   }
 }
